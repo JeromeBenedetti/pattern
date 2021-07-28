5c0d8536c08754fbd9fd97bf19edd597f5903353
JeromeBenedetti <benedetti.jerome@laposte.net>
2021-07-28 19:35:52 +0200

    feat(Post): add Post entity class
    To be able to publish articles on this blog, a 'Post' entity's class
    has been written

---
+++ src/Entity/Post.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use DateTime;
+use Exception;
+use phpDocumentor\Reflection\Types\Void_;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="posts")
+ * @ORM\HasLifecycleCallbacks
+ */
+class Post
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * 
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="message", type="string")
+     * 
+     * @var string
+     */
+    private $message;
+
+    /**
+     * @ORM\Column(name="created", type"datetime")
+     * 
+     * @var DateTime
+     */
+    private $created;
+
+    /**
+     * @ORM\Column(name="updated", type"datetime")
+     * 
+     * @var DateTime
+     */
+    private $updated;
+
+    /**
+     * @ORM\PrePersist
+     * 
+     * @throws Exception
+     */
+    public function onPrePersist(): void
+    {
+        $this->created = new DateTime('now');
+    }
+
+    /**
+     * @ORM\PreUpdate
+     * 
+     * @throws Exception
+     */
+    public function onPreUpdate(): void
+    {
+        $this->updated = new DateTime('now');
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of created
+     */
+    public function getCreated(): \DateTime
+    {
+        return $this->created;
+    }
+
+    /**
+     * Get the value of updated
+     */
+    public function getUpdated(): ?\DateTime
+    {
+        return $this->updated;
+    }
+}
